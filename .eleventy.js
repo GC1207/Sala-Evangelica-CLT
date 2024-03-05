620d74d0adaa8677f48e8fdb74f180a7ab15efa9
GC1207 <gil2000ch@hotmail.com>
2024-03-05 00:33:54 -0500

    initial 11ty/nunjuks conversion
    Partial work in converting html/css files into njk files for templating.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("./website-church/images");
+    eleventyConfig.addPassthroughCopy("./website-church/styles.css");
+
     return {
         dir: {
             input: "website-church",
