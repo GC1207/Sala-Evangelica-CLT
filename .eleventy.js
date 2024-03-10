36868138a379160097c32727bb8af5cc57e85e50
GC1207 <gil2000ch@hotmail.com>
2024-03-10 13:58:39 -0400

    finishing templating
    finishing templating for blog and blog posts.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./website-church/images");
     eleventyConfig.addPassthroughCopy("./website-church/styles.css");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);  
+    })
 
     return {
         dir: {
